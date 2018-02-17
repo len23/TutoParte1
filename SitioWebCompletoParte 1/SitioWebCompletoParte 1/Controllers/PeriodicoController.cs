c3f4c3b1e07165c28fd591172509f97ed1ba4c81
len23 <lenin.montalvo.23@hotmail.com>
2018-02-17 14:53:13 -0500

    Acabada la Parte 1 del video

--- SitioWebCompletoParte 1/SitioWebCompletoParte 1/Controllers/PeriodicoController.cs
+++ SitioWebCompletoParte 1/SitioWebCompletoParte 1/Controllers/PeriodicoController.cs
@@ -13,5 +13,10 @@ namespace SitioWebCompletoParte_1.Controllers
         {
             return View();
         }
+
+        public ActionResult ListaNoticias()
+        {
+            return View();
+        }
     }
 }
