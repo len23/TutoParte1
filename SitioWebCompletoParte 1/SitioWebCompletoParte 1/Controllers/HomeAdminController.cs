bd27440fa2f8e41cfeb15dd13292f1279985f447
len23 <lenin.montalvo.23@hotmail.com>
2018-02-17 15:55:56 -0500

    Completo con controlador y vista

---
+++ SitioWebCompletoParte 1/SitioWebCompletoParte 1/Controllers/HomeAdminController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SitioWebCompletoParte_1.Controllers
+{
+    public class HomeAdminController : Controller
+    {
+        // GET: HomeAdmin
+        public ActionResult Home()
+        {
+            ViewBag.encabezado = "Bienvenido Administrador";
+            return View();
+        }
+    }
+}
